6221f92abd47f15cf6fe10f60bfc8e2ac5c3d0f9
chinoll <chinoll@protonmail.com>
2021-05-07 22:06:49 +0800

    update

--- pjson.py
+++ pjson.py
@@ -18,18 +18,4 @@ def _dumps(c):
     return attr_dict
 
 def dumps(c):
-    x = dir(c)
-    attr_dict = {}
-    for i in x:
-        try:
-            if re.match('__[a-zA-Z_]*__',i):
-                continue
-            attr_dict[i] = getattr(c,i)
-        except:
-            pass
-    for i in attr_dict:
-        if type(attr_dict[i]) in [int,str,dict,list,set]:
-            continue
-        if isinstance(attr_dict[i],object):
-            attr_dict[i] = _dumps(attr_dict[i])
-    return json.dumps(attr_dict)
+    return json.dumps(_dumpss)
